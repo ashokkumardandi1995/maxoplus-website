f9f591031a86c35dd1efd1641da322585b9ffead
ashokkumardandi1995 <154795343+ashokkumardandi1995@users.noreply.github.com>
2026-06-27 21:24:40 +0530

    Update customers.js

--- js/customers.js
+++ js/customers.js
@@ -2,15 +2,45 @@ let customers = [];
 
 fetch("data/customers.json?v=" + Date.now())
 .then(res => res.json())
-.then(data => {
+.then(data=>{
 
     customers = data;
 
-    console.log("Customers Loaded:", customers);
+});
 
-})
-.catch(err => {
+const searchBox =
+document.getElementById("customerSearch");
 
-    console.log("Customer file not found", err);
+searchBox.addEventListener("input",function(){
+
+const value =
+this.value.toLowerCase().trim();
+
+const customer =
+customers.find(c=>
+
+c.name.toLowerCase().includes(value) ||
+
+c.phone.includes(value) ||
+
+c.gst.toLowerCase().includes(value)
+
+);
+
+if(customer){
+
+document.getElementById("customerName").value =
+customer.name;
+
+document.getElementById("customerPhone").value =
+customer.phone;
+
+document.getElementById("customerGST").value =
+customer.gst;
+
+document.getElementById("customerAddress").value =
+customer.address;
+
+}
 
 });
