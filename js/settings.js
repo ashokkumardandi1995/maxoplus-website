71f06a9f960f5eeadfd8d7aabf2c9ca6c54088f2
ashokkumardandi1995 <154795343+ashokkumardandi1995@users.noreply.github.com>
2026-06-27 22:36:05 +0530

    Add settings.js to fetch and display invoice number

---
+++ js/settings.js
@@ -0,0 +1,13 @@
+let settings = {};
+
+fetch("data/settings.json?v=" + Date.now())
+.then(res => res.json())
+.then(data=>{
+
+    settings = data;
+
+    document.getElementById("invoiceNo").value =
+    "INV-" +
+    String(settings.lastInvoice).padStart(5,"0");
+
+});
