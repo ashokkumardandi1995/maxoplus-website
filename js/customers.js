cc05f01c840801ec9ba0758fc2a08fb8143589b7
ashokkumardandi1995 <154795343+ashokkumardandi1995@users.noreply.github.com>
2026-06-27 19:26:27 +0530

    Update customers.js

--- js/customers.js
+++ js/customers.js
@@ -1,1 +1,16 @@
-q
+let customers = [];
+
+fetch("data/customers.json?v=" + Date.now())
+.then(res => res.json())
+.then(data => {
+
+    customers = data;
+
+    console.log("Customers Loaded:", customers);
+
+})
+.catch(err => {
+
+    console.log("Customer file not found", err);
+
+});
