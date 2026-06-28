6089f742b4b5a1d3885083d318d594e914a707b4
ashokkumardandi1995 <154795343+ashokkumardandi1995@users.noreply.github.com>
2026-06-28 11:02:30 +0530

    Create pdf.js

---
+++ js/pdf.js
@@ -0,0 +1,52 @@
+async function createPDF(invoice){
+
+const { jsPDF } = window.jspdf;
+
+const doc = new jsPDF("p","mm","a4");
+
+doc.setFont("helvetica","bold");
+
+doc.setFontSize(22);
+
+doc.text("TAX INVOICE",105,20,{align:"center"});
+
+doc.setFontSize(18);
+
+doc.setTextColor(0,74,173);
+
+doc.text("MAXO PLUS",20,35);
+
+doc.setFontSize(10);
+
+doc.setTextColor(80);
+
+doc.text("Premium Cleaning Solutions For Every Home",20,42);
+
+doc.text("Visakhapatnam, Andhra Pradesh",20,48);
+
+doc.text("Phone : +91 9493945946",20,54);
+
+doc.text("Email : maxopluscare@gmail.com",20,60);
+
+doc.setTextColor(0);
+
+doc.text("Invoice No : " + invoice.invoiceNo,140,35);
+
+doc.text("Date : " + invoice.invoiceDate,140,42);
+
+doc.line(20,68,190,68);
+
+doc.setFontSize(14);
+
+doc.text("Customer Details",20,78);
+
+doc.setFontSize(11);
+
+doc.text("Name : " + invoice.customerName,20,88);
+
+doc.text("Phone : " + invoice.customerPhone,20,95);
+
+doc.text("GST : " + invoice.customerGST,20,102);
+
+doc.text("Address : " + invoice.customerAddress,20,109);
+  const rows = [];
