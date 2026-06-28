476458bb1d4b9b4fedbef48997d0f7d1200ca5ff
ashokkumardandi1995 <154795343+ashokkumardandi1995@users.noreply.github.com>
2026-06-28 23:27:31 +0530

    Update history.js

--- js/history.js
+++ js/history.js
@@ -64,3 +64,58 @@ function viewInvoice(index){
 openInvoice(index);
 
 }
+document
+.getElementById("searchInvoice")
+.addEventListener("input", searchInvoice);
+
+function searchInvoice(){
+
+const value =
+document.getElementById("searchInvoice")
+.value.toLowerCase();
+
+tbody.innerHTML = "";
+
+invoices
+.filter(invoice =>
+
+invoice.invoiceNo.toLowerCase().includes(value) ||
+
+invoice.customerName.toLowerCase().includes(value)
+
+)
+
+.forEach((invoice,index)=>{
+
+const row =
+document.createElement("tr");
+
+row.innerHTML = `
+
+<td>${invoice.invoiceNo}</td>
+
+<td>${invoice.invoiceDate}</td>
+
+<td>${invoice.customerName}</td>
+
+<td>₹ ${invoice.grandTotal}</td>
+
+<td>
+
+<button onclick="viewInvoice(${index})">
+View
+</button>
+
+<button onclick="deleteInvoice(${index})">
+Delete
+</button>
+
+</td>
+
+`;
+
+tbody.appendChild(row);
+
+});
+
+}
