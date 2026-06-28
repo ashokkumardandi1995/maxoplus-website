3fc3727434a700bed5e5068ca188debfdad8570f
ashokkumardandi1995 <154795343+ashokkumardandi1995@users.noreply.github.com>
2026-06-28 14:53:38 +0530

    Update pdf.js

--- js/pdf.js
+++ js/pdf.js
@@ -147,7 +147,23 @@ doc.text("₹ 0.00",185,finalY+22,{align:"right"});
 
 doc.setFont("helvetica","bold");
 doc.text("₹ " + invoice.grandTotal,185,finalY+29,{align:"right"});
+doc.setFont("helvetica","bold");
+doc.setFontSize(10);
 
+doc.text(
+"Amount in Words",
+15,
+finalY+12
+);
+
+doc.setFont("helvetica","normal");
+doc.setFontSize(9);
+
+doc.text(
+numberToWords(Number(invoice.grandTotal)) + " Rupees Only",
+15,
+finalY+19
+);
 // =========================
 // Signature
 // =========================
@@ -189,5 +205,48 @@ doc.setTextColor(0);
 // =========================
 
 doc.save(invoice.invoiceNo + ".pdf");
+
+}
+function numberToWords(num){
+
+const a=[
+"",
+"One","Two","Three","Four","Five","Six","Seven","Eight","Nine",
+"Ten","Eleven","Twelve","Thirteen","Fourteen","Fifteen",
+"Sixteen","Seventeen","Eighteen","Nineteen"
+];
+
+const b=[
+"",
+"",
+"Twenty",
+"Thirty",
+"Forty",
+"Fifty",
+"Sixty",
+"Seventy",
+"Eighty",
+"Ninety"
+];
+
+if(num<20) return a[num];
+
+if(num<100)
+return b[Math.floor(num/10)]+" "+a[num%10];
+
+if(num<1000)
+return a[Math.floor(num/100)]+" Hundred "+numberToWords(num%100);
+
+if(num<100000)
+return numberToWords(Math.floor(num/1000))+
+" Thousand "+
+numberToWords(num%1000);
+
+if(num<10000000)
+return numberToWords(Math.floor(num/100000))+
+" Lakh "+
+numberToWords(num%100000);
+
+return num.toString();
 
 }
