41785eb9e3f9ce0897378885501202992a153ff6
ashokkumardandi1995 <154795343+ashokkumardandi1995@users.noreply.github.com>
2026-06-28 21:29:06 +0530

    Update history.js

--- js/history.js
+++ js/history.js
@@ -1,1 +1,46 @@
-a
+let invoices =
+JSON.parse(localStorage.getItem("maxoInvoices")) || [];
+
+const tbody =
+document.getElementById("historyBody");
+
+function loadInvoices(){
+
+tbody.innerHTML = "";
+
+invoices.forEach((invoice,index)=>{
+
+const row =
+document.createElement("tr");
+
+row.innerHTML = `
+
+<td>${invoice.invoiceNo}</td>
+
+<td>${invoice.invoiceDate}</td>
+
+<td>${invoice.customerName}</td>
+
+<td>₹ ${invoice.grandTotal}</td>
+
+<td>
+
+<button onclick="viewInvoice(${index})">
+View
+</button>
+
+<button onclick="deleteInvoice(${index})">
+Delete
+</button>
+
+</td>
+
+`;
+
+tbody.appendChild(row);
+
+});
+
+}
+
+loadInvoices();
