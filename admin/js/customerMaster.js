d352b641c63eebbe2648b751e684e7236ede6f60
ashokkumardandi1995 <154795343+ashokkumardandi1995@users.noreply.github.com>
2026-06-29 14:28:35 +0530

    Create customerMaster.js

---
+++ admin/js/customerMaster.js
@@ -0,0 +1,54 @@
+let customers =
+JSON.parse(localStorage.getItem("maxoCustomers")) || [];
+
+const tbody =
+document.getElementById("customerBody");
+
+function loadCustomers(){
+
+tbody.innerHTML="";
+
+customers.forEach((customer,index)=>{
+
+const row=document.createElement("tr");
+
+row.innerHTML=`
+
+<td>${customer.name}</td>
+
+<td>${customer.phone}</td>
+
+<td>${customer.gst}</td>
+
+<td>${customer.address}</td>
+
+<td>
+
+<button onclick="editCustomer(${index})">
+
+Edit
+
+</button>
+
+<button onclick="deleteCustomer(${index})">
+
+Delete
+
+</button>
+
+</td>
+
+`;
+
+tbody.appendChild(row);
+
+});
+
+}
+
+loadCustomers();
+function editCustomer(index){
+
+    alert(customers[index].name);
+
+}
