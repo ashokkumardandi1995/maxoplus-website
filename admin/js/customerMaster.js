7947cb0b86a5fd2da576c7cdbc0b328755cc704d
ashokkumardandi1995 <154795343+ashokkumardandi1995@users.noreply.github.com>
2026-06-29 15:00:08 +0530

    Update customerMaster.js

--- admin/js/customerMaster.js
+++ admin/js/customerMaster.js
@@ -53,3 +53,59 @@ function editCustomer(index){
     alert(customers[index].name);
 
 }
+document
+.getElementById("searchCustomer")
+.addEventListener("input", searchCustomer);
+
+function searchCustomer(){
+
+const value =
+document.getElementById("searchCustomer")
+.value.toLowerCase();
+
+tbody.innerHTML = "";
+
+customers
+.filter(customer =>
+
+customer.name.toLowerCase().includes(value) ||
+
+customer.phone.includes(value) ||
+
+customer.gst.toLowerCase().includes(value)
+
+)
+
+.forEach((customer,index)=>{
+
+const row = document.createElement("tr");
+
+row.innerHTML = `
+
+<td>${customer.name}</td>
+
+<td>${customer.phone}</td>
+
+<td>${customer.gst}</td>
+
+<td>${customer.address}</td>
+
+<td>
+
+<button onclick="editCustomer(${index})">
+Edit
+</button>
+
+<button onclick="deleteCustomer(${index})">
+Delete
+</button>
+
+</td>
+
+`;
+
+tbody.appendChild(row);
+
+});
+
+}
